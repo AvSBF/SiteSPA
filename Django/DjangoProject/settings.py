3d8c3c95ab54a255e4bc631f093812c2ffd1fc35
AvS <avsbf033@gmail.com>
2022-05-27 12:15:21 +0300

    hid key

--- Django/DjangoProject/settings.py
+++ Django/DjangoProject/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
+from .secret_config import SECRET_KEY
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +23,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*_kc8vt$xm168(9wpo8iqb+qv(@f04x7#lhfscj@(&9ay(9f73'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
